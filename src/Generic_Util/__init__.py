6712e9eded47a4f922fa6a9d37255a5a6d767d8d
T-Flet <T-Fletcher@outlook.com>
2023-04-19 19:09:32 +0100

    Assembled functions into a package

---
+++ src/Generic_Util/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 Thomas Fletcher. All rights reserved.
+
+generic-util: A collection of frequently used (mostly functional) utilities not found in the standard library
+"""
+
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
+
+__all__ = ("__version__",)
